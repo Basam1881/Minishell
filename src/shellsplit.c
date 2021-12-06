02089eccfa3f4e4df3eb5064ea3fd6d33dd123da
Bassam Naji <bnaji@student.42abudhabi.ae>
2021-12-06 12:50:11 +0400

    fixed some issues on 2d split

--- src/shellsplit.c
+++ src/shellsplit.c
@@ -6,7 +6,7 @@
 /*   By: bnaji <bnaji@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 14:19:25 by dfurneau          #+#    #+#             */
-/*   Updated: 2021/12/06 03:21:15 by bnaji            ###   ########.fr       */
+/*   Updated: 2021/12/06 12:49:21 by bnaji            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,37 +31,38 @@ static void	ft_free(char **str, int n)
 	It's used to set the qoutes flags either to 1 or 0, so we
 	can tell if we are inside qoutes at the moment or not
 */
-static void	check_arg_helper1(char *s, int *x, int *single_qoute, int *double_qoute)
+static void	qoutes_checker(char **str, char *s, int *x, int *single_qoute, int *double_qoute, int flag, int *i, int *j)
 {
-	if (s[*x] == '\'' || s[*x] == '"')
+	if ((s[*x] == '\'' || s[*x] == '"') && !*single_qoute && !*double_qoute)
 	{
-		if (s[*x] == '\'' || s[*x] == '"')
-		{
-			if (s[*x] == '\'')
-				*single_qoute = 1;
-			else
-				*double_qoute = 1;
-			(*x)++;
-		}
+		if (s[*x] == '\'')
+			*single_qoute = 1;
+		else
+			*double_qoute = 1;
+		(*x)++;
 	}
-	if ((s[*x] == '\'' && single_qoute) || (s[*x] == '"' && double_qoute))
+	if ((s[*x] == '\'' && *single_qoute) || (s[*x] == '"' && *double_qoute))
 	{
-		if ((s[*x] == '\'' && single_qoute) || (s[*x] == '"' && double_qoute))
-		{
-			if (s[*x] == '\'')
-				*single_qoute = 0;
-			else
-				*double_qoute = 0;
-			(*x)++;
-		}
+		if (s[*x] == '\'')
+			*single_qoute = 0;
+		else
+			*double_qoute = 0;
 	}
+	else
+	{
+		if (flag == 2)
+			str[*i][*j] = s[*x];
+		if (flag)
+			(*j)++;
+	}
+	(*x)++;
 }
 
 /*
 	It's used to either malloc the size for the argument or
 	to put null at the end according the to the flag
 */
-static void	check_arg_helper2(char **str, int *i, int *j, int flag)
+static void	check_arg_helper(char **str, int *i, int *j, int flag)
 {
 	if (flag)
 	{
@@ -92,26 +93,18 @@ static void	check_arg(char **str, char *s, int flag, int *x, int *i)
 	j = 0;
 	if (!flag)
 		(*i)++;
-	while (s[*x] && (s[*x] != ' ' || (s[*x] == ' ' && (single_qoute || double_qoute))))
-	{
-		check_arg_helper1(s, x, &single_qoute, &double_qoute);
-		if (!((s[*x] == '\'' && single_qoute) || (s[*x] == '"' && double_qoute)))
-		{
-			if (flag == 2)
-				str[*i][j] = s[*x];
-			if (flag)
-				j++;
-		}
-		(*x)++;
-	}
-	check_arg_helper2(str, i , &j, flag);
+	while (s[*x] && (s[*x] != ' ' || (s[*x] == ' '
+				&& (single_qoute || double_qoute))))
+		qoutes_checker(str, s, x, &single_qoute, &double_qoute, flag, i, &j);
+	check_arg_helper(str, i, &j, flag);
 }
 
-/*
+	/*
 	The function has three integer flags (0, 1 and 2)
 	flag 0 will give the number of words (arguments)
 	flag 1 will allocate a memory for the words (arguments)
-	flag 2 will copy the proper value from the string (cmdline) to the arguments you have
+	flag 2 will copy the proper value from the string (cmdline)
+	to the arguments you have
 */
 static int	split_into_arg(char **str, char *s, int flag)
 {
@@ -138,7 +131,6 @@ static int	split_into_arg(char **str, char *s, int flag)
 	return (i);
 }
 
-
 /*It's used to seperate the string (command) into arguments,
 	store them into 2d array and return it back*/
 char	**cmd_split(char *cmdline)
@@ -156,7 +148,3 @@ char	**cmd_split(char *cmdline)
 	split_into_arg(str, (char *)cmdline, 2);
 	return (str);
 }
-
-// int main(){
-// 	cmd_split();
-// }
